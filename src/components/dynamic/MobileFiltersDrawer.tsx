3f57977b5fc9652796a313e705bb637260dfdad4
Lucas Hott <drlucashott@gmail.com>
2026-03-17 00:29:51 -0300

    update

--- src/components/dynamic/MobileFiltersDrawer.tsx
+++ src/components/dynamic/MobileFiltersDrawer.tsx
@@ -69,7 +69,6 @@ export function MobileFiltersDrawer({ brands, sellers, dynamicConfigs, dynamicOp
     });
 
     router.push(`?${params.toString()}`);
-    router.refresh(); // 🚀 CORREÇÃO: Força o servidor a atualizar a página na Vercel
     setOpen(false);
   };
 
